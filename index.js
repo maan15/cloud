ee012cf7c83768321b8dc792282da3a3a085b7d4
maan15 <farmanchaudhary15@gmail.com>
2024-08-04 15:56:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Url = 'https://kzepafm5l42qdm4ta272jxqt2u0iclsz.lambda-url.us-east-1.on.aws/';
+const counter = document.querySelector(".views");
+async function updateCounter() {
+    try {
+        let response = await fetch(Url);
+                if (!response.ok) {
+            throw new Error(`Network response was not ok. Status: ${response.status}`);
+        }
+        let data = await response.text();
+        console.log('API Response:', data);
+        counter.innerHTML = `This page has ${data} views!`;
+
+    } catch (error) {
+        console.error('Error:', error);
+        counter.innerHTML = `Failed to retrieve view count. Error: ${error.message}`;
+    }
+}
+updateCounter();
